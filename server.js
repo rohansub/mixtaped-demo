5399db907d81b5008bf4a1f139507897b7d13b71
Rohan Subramaniam <rcsubra2@illinois.edu>
2017-04-16 17:57:01 -0500

    clean up

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 // silly chrome wants SSL to do screensharing
-var fs = require('fs'),
-    express = require('express'),
-    https = require('https'),
+var express = require('express'),
     http = require('http');
-
-
-var privateKey = fs.readFileSync('fakekeys/privatekey.pem').toString(),
-    certificate = fs.readFileSync('fakekeys/certificate.pem').toString();
-
 
 var app = express();
 
 app.use(express.static(__dirname));
 
-https.createServer({key: privateKey, cert: certificate}, app).listen(8000);
-http.createServer(app).listen(8001);
+http.createServer(app).listen(8000);
 
-console.log('running on https://localhost:8000 and http://localhost:8001');
+console.log('running on http://localhost:8000');
