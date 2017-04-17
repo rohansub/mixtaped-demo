0fa3c25f1234d912e2c02650046c38b28fbc5f99
Kailang Fu <kailangfu@gmail.com>
2017-04-17 09:52:00 -0500

    open https on 443

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // silly chrome wants SSL to do screensharing
-var express = require('express'),
-	http = require('http');
+var fs = require('fs'),
+	express = require('express'),
+	http = require('http'),
+	https = require('https');
 
 var app = express();
-
 app.use(express.static(__dirname));
-
 http.createServer(app).listen(80);
-
 console.log('running on http://localhost:80');
+
+try {
+	var privateKey = fs.readFileSync('/etc/letsencrypt/keys/0000_key-certbot.pem').toString(),
+		certificate = fs.readFileSync('/etc/letsencrypt/csr/0000_csr-certbot.pem').toString();
+	https.createServer({key: privateKey, cert: certificate}, app).listen(443);
+	console.log('running on https://localhost:443');
+} catch (err) {
+	console.log('cannot start https server');
+}
