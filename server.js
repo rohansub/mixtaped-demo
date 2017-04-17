860a98450ef21bc8a3c473249f28e3abf85584a9
Kailang Fu <kailangfu@gmail.com>
2017-04-17 11:03:30 -0500

    fix key and cert

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ http.createServer(app).listen(80);
 console.log('running on http://localhost:80');
 
 try {
-	var privateKey = fs.readFileSync('/etc/letsencrypt/keys/0000_key-certbot.pem'),
-		certificate = fs.readFileSync('/etc/letsencrypt/csr/0000_csr-certbot.pem');
-	console.log(privateKey, certificate);
+	var privateKey = fs.readFileSync('/etc/letsencrypt/live/touhou.live/privkey.pem'),
+		certificate = fs.readFileSync('/etc/letsencrypt/live/touhou.live/cert.pem');
+	console.log(privateKey);
+	console.log(certificate);
 	
 	https.createServer({key: privateKey, cert: certificate}, app).listen(443);
 	console.log('running on https://localhost:443');
 } catch (err) {
-	console.log('cannot start https server', err);
+	console.log('cannot start https server\n', err);
 }
