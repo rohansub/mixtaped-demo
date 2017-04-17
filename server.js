31e4bc1f1c68ae389ee8778bf1d13f09f2da8483
Rohan Subramaniam <rcsubra2@illinois.edu>
2017-04-17 16:17:52 -0500

    added server routing functionality

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ var express = require('express'),
 var app = express();
 
 app.use(express.static(__dirname));
-
+app.engine('.html', require('ejs').__express);
 http.createServer(app).listen(8000);
+
+app.get("/broadcast", function(req, res) {
+    res.render(__dirname + "/broadcast.html");
+});
+
+app.get("/watch/:tagId", function(req, res) {
+	res.render(__dirname + "/watch.html", {
+		user: req.params.tagId
+	});
+});
+
+
 
 console.log('running on http://localhost:8000');
