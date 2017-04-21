fe93d0fe3ae23e55b77e654c1f9ca27d68fb15ce
Rohan Subramaniam <rcsubra2@illinois.edu>
2017-04-21 18:48:51 -0500

    secured https

--- server.js
+++ server.js
@@ -5,21 +5,16 @@ var fs = require('fs'),
 	http = require('http'),
 	https = require('https');
 
-var redirectApp = express () ,
-redirectServer = http.createServer(redirectApp);
-redirectApp.use(function requireHTTPS(req, res, next) {
-  	if (!req.secure) {
-	  	return res.redirect('https://' + req.headers.host + req.url);
-	}
-	next();
-})
-redirectServer.listen(80);
 
 
 var app = express();
+
+app.all('*', ensureSecure); // at top of routing calls
+
 app.use(express.static(__dirname));
+
 app.engine('.html', require('ejs').__express);
-// http.createServer(app).listen(80);
+http.createServer(app).listen(80);
 console.log('running on http://localhost:80');
 http.get('*',function(req,res){
     res.redirect('https://localhost:443'+req.url)
@@ -46,3 +41,13 @@ app.get("/watch/:tagId", function(req, res) {
 		user: req.params.tagId
 	});
 });
+
+function ensureSecure(req, res, next){
+  if(req.secure){
+    // OK, continue
+    return next();
+  };
+  // handle port numbers if you need non defaults
+  // res.redirect('https://' + req.host + req.url); // express 3.x
+  res.redirect('https://' + req.hostname + req.url); // express 4.x
+}
