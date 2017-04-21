914626538e04d0d50f176c4df865a33dcb2f973b
Rohan Subramaniam <rcsubra2@illinois.edu>
2017-04-21 16:48:26 -0500

    front-end improved and integrated

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ var fs = require('fs'),
 var app = express();
 app.use(express.static(__dirname));
 app.engine('.html', require('ejs').__express);
-http.createServer(app).listen(80);
-console.log('running on http://localhost:80');
-
-try {
-	var privateKey = fs.readFileSync('/etc/letsencrypt/live/touhou.live/privkey.pem'),
-		certificate = fs.readFileSync('/etc/letsencrypt/live/touhou.live/cert.pem');
-	// console.log(privateKey);
-	// console.log(certificate);
-
-	https.createServer({key: privateKey, cert: certificate}, app).listen(443);
-	console.log('running on https://localhost:443');
-} catch (err) {
-	console.log('cannot start https server\n', err);
-}
+http.createServer(app).listen(8000);
+// console.log('running on http://localhost:80');
+//
+// try {
+// 	var privateKey = fs.readFileSync('/etc/letsencrypt/live/touhou.live/privkey.pem'),
+// 		certificate = fs.readFileSync('/etc/letsencrypt/live/touhou.live/cert.pem');
+// 	// console.log(privateKey);
+// 	// console.log(certificate);
+//
+// 	https.createServer({key: privateKey, cert: certificate}, app).listen(443);
+// 	console.log('running on https://localhost:443');
+// } catch (err) {
+// 	console.log('cannot start https server\n', err);
+// }
 
 
 app.get("/broadcast", function(req, res) {
